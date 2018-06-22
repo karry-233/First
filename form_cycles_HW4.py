7d16c6989f492e79bd4373d7fed8c73449818c5b
karry-233 <35772944+karry-233@users.noreply.github.com>
2018-06-22 09:23:50 +0900

    Add files via upload

---
+++ form_cycles_HW4.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 16:39:25 2018
+
+@author: karry
+"""
+
+
+"""input a word to find all the cycles it can form within n nodes"""
+
+
+import networkx as nx 
+
+
+def build_graph():
+    graph = nx.DiGraph()
+    word_index = {}
+    
+    
+    #add nodes to graph
+    Indexs=open("pages.txt","r")
+    for line in Indexs:
+        Index_wikiWord = line.split()
+        graph.add_node(Index_wikiWord[0])  
+        word_index[Index_wikiWord[1]] = Index_wikiWord[0]
+        # node is the index, not word
+        # word_index = {"word":"index"}
+    Indexs.close()
+    
+    #add edges to graph
+    Links=open("links.txt","r")
+    for line in Links:
+        link = line.split()
+        graph.add_edge(link[0],link[1])
+    Links.close()
+    
+    return graph, word_index
+
+
+# Confirm inputword exists in wikipedia and return the index of the inputword
+def inputword_search(inputword):
+    try:    
+        index_of_inputword = word_index.get(inputword)
+        print(inputword, "exists in wikipedia.")
+        return index_of_inputword
+    except:
+        print(inputword, "is not in wikipedia.")
+        print("Please try again.")
+        exit(0)
+    
+    
+def form_cycles(inputword,n):
+    index_of_inputword = inputword_search(inputword)
+    cycles = list(nx.all_simple_paths(graph,index_of_inputword,index_of_inputword,n)) 
+    # find all the cycles that includes less than n nodes 
+    if cycles == []:
+        print("No cycle is found.")
+    else:
+        for cycle in cycles:
+            print("Here is the cycle")
+            for node in cycle:
+                for word, index in word_index.items():   
+                    if index == node:
+                        print(word, "->")
+            print("-------------------")
+                   
+    
+graph, word_index = build_graph()
+inputword = input("Please enter a word:")
+n = input("Please decide the maximum size of cycles:")
+form_cycles(inputword,n)
+# n = 3,4,5...
