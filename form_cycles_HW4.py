8d5fa612f458d6bcd64b4605178061943daa3ef3
karry-233 <35772944+karry-233@users.noreply.github.com>
2018-06-22 09:26:49 +0900

    HW4

--- form_cycles_HW4.py
+++ form_cycles_HW4.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
-Created on Wed Jun 20 16:39:25 2018
-
-@author: karry
+input a word to find all the cycles it can form within n times
+(using the link within n times, to find out all the ways to get back to the original word)
 """
-
-
-"""input a word to find all the cycles it can form within n nodes"""
 
 
 import networkx as nx 
